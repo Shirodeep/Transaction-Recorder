4039738521336872e2089fe741ceacbbda4a1c0c
npoudelp <npoudelp@gmail.com>
2024-06-02 16:19:13 +0545

    media url for rendering in url patterns added

--- loanrecorder/urls.py
+++ loanrecorder/urls.py
@@ -30,6 +30,4 @@ urlpatterns = [
 
 ]
 
-# for image uncomment
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
